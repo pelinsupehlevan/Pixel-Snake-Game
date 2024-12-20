ba91767df04cc098fa04c39dc418fa016b3b2b4e
melikkomsal <melik.komsal@ozu.edu.tr>
2024-12-20 22:11:43 +0300

    Update Snake.py
    deneme

--- model/Snake.py
+++ model/Snake.py
@@ -2,19 +2,3 @@ from tkinter import *
 import random
 
 SPACE_SIZE = 50
-BODY_PARTS = 3
-SNAKE_COLOR = "green"
-
-class Snake:
-
-    def __init__(self):
-        self.body_size = BODY_PARTS
-        self.coordinates = []
-        self.squares = []
-
-        for i in range(0, BODY_PARTS):
-            self.coordinates.append([0, 0])
-
-        for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
-            self.squares.append(square)
